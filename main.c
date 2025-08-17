2fa73314a4e466cbf4ba19a84db7577ab2d5e6bc
Rhyloo <rhyloot@gmail.com>
2025-08-17 17:16:51 +0200

    FileReader + FileParsing.
    FileReader opens a file and reads its contents.
    FileParsing does basic parsing letter by letter.

--- main.c
+++ main.c
@@ -7,59 +7,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "tokens.h"
 
+#define TOKEN_COMMENT '-'
 
-#ifdef DEBUG  
-#define DEBUG(...) printf(__VA_ARGS__)
-#else
-#define DEBUG(...) do {} while (0)
-#endif
+static char *raw;
 
-static void readin(char *file){
-  int fd;
-  struct stat st;
-  char *p;
+static void FileReader(char *file){
+  int file_descriptor;
+  struct stat stats;
 
-  DEBUG("Nombre del archivo: %s\n",file);
-  DEBUG("Extensión: %s\n",strrchr(file, '.'));
-    
-  if (strrchr(file, '.') == NULL)
-    error("file must end in '.vhdl'");
-  
-  if (!!strcmp(strrchr(file, '.'), ".vhdl"))
-    error("file must end in '.vhdl'");
+  file_descriptor = open(file, O_RDONLY, 0);
+  fstat(file_descriptor, &stats);
+  raw = malloc(stats.st_size + 1);
+  read(file_descriptor, raw, stats.st_size);
+  raw[stats.st_size] = EOF;
+  (void) close(file_descriptor);
+}
 
-  if ((fd = open(file, O_RDONLY)) == -1)
-    error("couldn't open %s", file);
-
-  if (fstat(fd, &st) == -1)
-    error("couldn't get file size");
-
-  if ((raw = malloc(st.st_size + 1)) == NULL)
-    error("malloc failed");
-
-  if (read(fd, raw, st.st_size) != st.st_size)
-    error("couldn't read %s", file);
-  raw[st.st_size] = '\0';
-
-  (void) close(fd);
+static void FileParsing(){
+  while(*raw != EOF){
+    printf("%c\n",*raw);
+    if (*raw == TOKEN_COMMENT){
+      printf("Comment detected!\n");
+    }
+    *raw++;
+  }
 }
 
 int main(int argc, char *argv[]){
-  char *startp;
 
   if (argc != 2) {
     (void) fputs("usage: pl0c file.pl0\n", stderr);
     exit(1);
   }
 
-  readin(argv[1]);
-  startp = raw;
-
-  parse();
-
-  free(startp);
+  FileReader(argv[1]);
+  FileParsing();
 
   return 0;
 }
