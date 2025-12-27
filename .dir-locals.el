1c16592790d6a60e0a67d4d8c780c9d282f580c1
rhyloo <rhyloot@gmail.com>
2025-12-27 16:05:43 +0100

    Update: Change in .gitignore file & .dir-locals modified.

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((c-mode . ((eval . (setq-local my/c-eldoc-includes
-                             (list (concat (projectile-project-root) "include"))))))
- (c++-mode . ((eval . (setq-local my/c-eldoc-includes
-                               (list (concat (projectile-project-root) "include")))))))
